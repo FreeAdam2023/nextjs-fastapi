8c4a7953972ea964bef13a4b3ec3664b199bb089
Adam Lyu <flyu008@uottawa.ca>
2024-11-01 21:23:30 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
+  async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+        destination: process.env.API_URL || "http://127.0.0.1:8000/api/:path*",
       },
     ];
   },
+  // 其他配置项
 };
 
 module.exports = nextConfig;
