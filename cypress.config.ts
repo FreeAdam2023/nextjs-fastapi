d9da89c42870c7aba8b1b46dceaa514e3508b346
Adam Lyu <flyu008@uottawa.ca>
2024-11-09 10:00:02 -0500

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,1 @@
-import { defineConfig } from "cypress";
 
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
